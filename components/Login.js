7dd8f7f2e4acc2c108499003743b62d966c5b86f
jackcrane <jack-mac@users.noreply.github.com>
2022-03-18 10:20:11 -0400

    Fixed auth (?)

--- components/Login.js
+++ components/Login.js
@@ -3,6 +3,7 @@ import { TouchableOpacity, Text, View, Linking, Image } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import { StatusBar } from "expo-status-bar";
 import AppLoading from "expo-app-loading";
+import Constants from "expo-constants";
 
 import { Storage, Spacer } from "./util/Utilities";
 
@@ -10,21 +11,30 @@ import styles from "./styles/LoginStyles";
 
 const Login = (props) => {
   const _openAuthSessionAsync = async () => {
+    console.log(Constants.linkingUri);
     Linking.addEventListener("url", handleOpenURL);
     const authsession = await WebBrowser.openAuthSessionAsync(
-      "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=137df427aa623a809915c5b88bd82df3cdbdb35f33cdee89e3d4b08b50178a9b&redirect_uri=https://docn-app-redirect.jackcrane.workers.dev&response_type=code&scope=write"
+      `https://cloud.digitalocean.com/v1/oauth/authorize?client_id=137df427aa623a809915c5b88bd82df3cdbdb35f33cdee89e3d4b08b50178a9b&redirect_uri=${encodeURIComponent(
+        "https://docn-app-redirect.jackcrane.workers.dev/?uri=$" +
+          encodeURIComponent(Constants.linkingUri)
+      )}&response_type=code&scope=write`
     );
     if (authsession.type === "success") {
       console.log("success");
       console.log("session", authsession.url);
       let authcode = authsession.url.split("code=")[1];
+      console.log("authcode", authcode);
       try {
         let response = await fetch(
-          `https://cloud.digitalocean.com/v1/oauth/token?client_id=137df427aa623a809915c5b88bd82df3cdbdb35f33cdee89e3d4b08b50178a9b&client_secret=75654094aa613a5435accf8b54ec82cce6839065deacab0a3438f5737c59c394&grant_type=authorization_code&code=${authcode}&redirect_uri=https://docn-app-redirect.jackcrane.workers.dev`,
+          `https://cloud.digitalocean.com/v1/oauth/token?client_id=137df427aa623a809915c5b88bd82df3cdbdb35f33cdee89e3d4b08b50178a9b&client_secret=75654094aa613a5435accf8b54ec82cce6839065deacab0a3438f5737c59c394&grant_type=authorization_code&code=${authcode}&redirect_uri=${encodeURIComponent(
+            "https://docn-app-redirect.jackcrane.workers.dev/?uri=$" +
+              encodeURIComponent(Constants.linkingUri)
+          )}`,
           {
             method: "POST",
           }
         );
+        console.log(response);
         let json = await response.json();
         if (json.access_token) {
           await fetch(
@@ -35,6 +45,9 @@ const Login = (props) => {
           await Storage.set("do_key", json.access_token);
           await Storage.set("do_refresh_key", json.refresh_token);
           props.route.params.AllowLogin();
+        } else {
+          console.log("Login failed");
+          console.log(json);
         }
       } catch (error) {
         console.error(error);
